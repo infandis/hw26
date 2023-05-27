15e76572687ec1f82939c923d57a6de055422784
infandis <svetlana290699@gmail.com>
2023-05-28 00:19:50 +0300

    did the task

--- main.js
+++ main.js
@@ -15,119 +15,121 @@ document.getElementById("products").addEventListener("click", function (event) {
         if (shown) {
             shown.classList.remove("shown");
         }
-        
+
         let infoId = event.target.getAttribute("id") + "-info";
         let info = document.getElementById(infoId);
         if (info) {
             info.classList.add("shown");
         }
-        
+
         document.getElementById("buy").classList.add("shown");
     }
 });
 
 //
 const form = document.getElementById("form");
-document.getElementById("buy").addEventListener("click", function(event) {
-    form.classList.add("shown");   
+document.getElementById("buy").addEventListener("click", function (event) {
+    form.classList.add("shown");
 });
 
 //
 const ordersWrapper = document.querySelector('.orders-wrapper');
 const shopWrapper = document.querySelector('.shop-wrapper');
 const ordersContainer = document.getElementById("orders-container");
-document.getElementById("orders-btn").addEventListener("click", function(event) {
-    ordersContainer.innerHTML = "";
+document.getElementById("orders-btn").addEventListener("click", function (event) {
     shopWrapper.classList.remove("shown");
     ordersWrapper.classList.add("shown");
-    let orders = listOrders();
-    for (let i in orders) {
-        let order = orders[i];
-        let div = document.createElement("div");
-        div.classList.add("order-title");
-        div.textContent = order.title;
-        ordersContainer.appendChild(div);
-        
-        let innerDiv = document.createElement("div");
-        innerDiv.classList.add("order-details");
-        div.appendChild(innerDiv);
-        innerDiv.innerHTML = `ПІБ: ${order.pib}<br/>Населений пункт: ${order.city}<br/>`
-        div.addEventListener("click", function(event) {
-            innerDiv.classList.toggle("shown");
-        })
-    }
+    showOrders();
 });
 
-document.getElementById("return-btn").addEventListener("click", function(event) {
+document.getElementById("return-btn").addEventListener("click", function (event) {
     ordersWrapper.classList.remove("shown");
     shopWrapper.classList.add("shown");
 })
 
 //
-document.getElementById("submit").addEventListener("click", function(event) {
-    if(form.checkValidity()) {
+document.getElementById("submit").addEventListener("click", function (event) {
+    if (form.checkValidity()) {
         let titleElement = document.querySelector(".product-info.shown .title");
+        let priceElement = document.querySelector(".product-info.shown .price");
         let order = {
-            id : Math.random().toString(36).substring(2),
+            id: Math.random().toString(36).substring(2),
             title: titleElement ? titleElement.textContent : "товар",
-            pib : document.getElementById("pib").value,
-            city : document.getElementById("city").value,
-            post : document.getElementById("NP").value,
-            date : Date.now()
+            price: priceElement ? priceElement.textContent : "безкоштовно",
+            pib: document.getElementById("pib").value,
+            city: document.getElementById("city").value,
+            post: document.getElementById("NP").value,
+            date: new Date().toLocaleDateString()
         };
-         
-        alert("Ви успішно придбали " + order.title + ". Товар буде доставлено в "  + order.city + ",  відділення Нової пошти номер " + order.post);
-        hide(); 
+
+        addOrder(order);
+        alert("Ви успішно придбали " + order.title + ". Товар буде доставлено в " + order.city + ",  відділення Нової пошти номер " + order.post);
+        hide();
     } else {
-        alert("Необхідно вказати дані для відправки");  
-    }   
+        alert("Необхідно вказати дані для відправки");
+    }
 })
 
 function hide() {
     document.querySelectorAll("#products .shown, #info .shown, #buy.shown")
-        .forEach(function(element) {
+        .forEach(function (element) {
             element.classList.remove("shown");
         });
 }
 
+function showOrders() {
+    ordersContainer.innerHTML = "";
 
-let saveOrder = function(order) {
+    let orders = getOrders();
+    for (let i in orders) {
+        let order = orders[i];
 
+        let wrapper = document.createElement("div");
+        wrapper.classList.add("order");
+
+        let div = document.createElement("div");
+        div.classList.add("order-title");
+        div.innerHTML = `<span>${order.date}</span>&nbsp;<strong>${order.title}</strong>&nbsp;<span>${order.price}</span>`;
+        div.addEventListener("click", function (event) {
+            wrapper.classList.toggle("shown");
+        })
+        wrapper.appendChild(div);
+
+        let innerDiv = document.createElement("div");
+        innerDiv.classList.add("order-details");
+        wrapper.appendChild(innerDiv);
+        innerDiv.innerHTML = `ПІБ: ${order.pib}<br/>Населений пункт: ${order.city}<br/>Відділення нової пошти: ${order.post}<br/>`;
+
+        let removeBtn = document.createElement("button");
+        removeBtn.textContent = "Видалити";
+        removeBtn.classList.add("remove-order");
+        removeBtn.addEventListener("click", function (event) {
+            deleteOrder(order.id);
+            showOrders();
+        });
+        innerDiv.appendChild(removeBtn);
+
+        ordersContainer.appendChild(wrapper);
+    }
+}
+
+let addOrder = function (order) {
+    localStorage.setItem(order.id, JSON.stringify(order));
 };
 
-let listOrders = function() {
-    return [
-        {
-            id : '111',
-            title: 'order1',
-            pib : 'sveta',
-            city : 'chern',
-            post : '13',
-            date : Date.now() 
-        },
 
-        {
-            id : '112',
-            title: 'order2',
-            pib : 'shani',
-            city : 'chern',
-            post : '13',
-            date : Date.now()
-        },
-        {
-            id : '113',
-            title: 'order3',
-            pib : 'irusa',
-            city : 'chern',
-            post : '13',
-            date : Date.now()  
-        }
-    ];
+let getOrders = function () {
+    let orders = [];
+    let keys = Object.keys(localStorage);
+    keys.forEach(function (key) {
+        let json = localStorage.getItem(key);
+        let value = JSON.parse(json);
+        orders.push(value);
+    });
+    return orders;
 };
 
-
-
-    
-    
-
+function deleteOrder(orderId) {
+    localStorage.removeItem(orderId);
+}
 
